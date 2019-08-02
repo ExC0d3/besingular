3556a9cd84d8b53ea98aee8050f55ada8a1213c8
Abhinav Sharma <sharma.abhinav503@gmail.com>
2019-08-02 00:28:25 -0700

    Adding contact forms

---
+++ thankyou.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="robots" content="noindex, nofollow">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>BeSingular</title>
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="img/favicon.ico">
+
+<!-- Custom CSS -->
+<link href="css/half-slider.css" rel="stylesheet">
+<!-- Custom Fonts -->
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Allura|Libre+Baskerville" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+
+<!-- Global site tag (gtag.js) - Google Ads: 735983414 -->
+<!-- Event snippet for Lead_BeSingular conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-735983414/sIjfCP_RnqYBELbu-N4C'});
+</script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-143002579-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-143002579-1');
+</script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-735983414"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-735983414');
+</script>
+
+</head>
+
+<body>
+
+
+
+<div class="row top-bar">
+ <div class="container">
+  <div class="col-md-6">
+    <a href="http://www.besingular.com/future-technologies.php"><img src="img/logo.png" class="img-responsive"></a>
+  </div>
+  
+  <div class="col-md-6">
+   <ul>
+    <li><a href="tel:9910524422"><I class="fa fa-phone"></i> +91 9910524422</a></li>
+    <li><a href="mailto:contact@besingular.com"><I class="fa fa-envelope" style="background:#47489e;"></i> contact@besingular.com</a></li>
+   </ul>
+  </div>
+  
+ </div>
+</div>
+
+
+
+
+<div class="row contactbx">
+  
+   <div class="container text-center mt50">
+         
+          
+          
+           <h1 style="font-size:50px;"><strong>Thank You!</strong></h1>
+           
+           <h3 style="margin-top:0px;">Your query is received and we will contact you soon.</h3>
+           
+           <a href="index.html" class="btn btnsubmit">GO TO HOMEPAGE</a>
+          
+         </div>
+  
+  </div>
+
+ 
+
+ <script src="js/jquery.js"></script>
+
+ <script src="js/bootstrap.min.js"></script>
+    
+
+</body>
+
+</html>
